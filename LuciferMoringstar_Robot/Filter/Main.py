4a7481d8bfd156a6c82fa45461ac0f4c0389e22d
mhjoyplex <109623346+mhjoyplex@users.noreply.github.com>
2022-07-20 22:47:32 +0600

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -81,6 +81,9 @@ async def filter(client, message):
             buttons.append(
                 [InlineKeyboardButton(text="📃 Pages 1/1",callback_data="pages")]
             )
+            buttons.append(
+                [InlineKeyboardButton(text="⚡ Powered By MOVIE WALA",callback_data="none")]
+            )            
             poster=None
             if API_KEY:
                 poster=await get_poster(search)
