21dee3e9bbfa912598e2e689dda2318f02af75b1
mhjoyplex <109623346+mhjoyplex@users.noreply.github.com>
2022-07-20 23:54:24 +0600

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -53,7 +53,7 @@ async def filter(client, message):
     if 2 < len(message.text) < 100:    
         btn = []
         search = message.text
-        mo_tech_yt = f"**🗂️ Title:** {search}\n**👤 Requested By:** {message.chat.id}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**"
+        mo_tech_yt = f"**🗂️ Title:** {search}\n**👤 Requested By:** {message.chat.id}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.user.title}**"
         files = await get_filter_results(query=search)
         if files:
             for file in files:
