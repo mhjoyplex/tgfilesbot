3fad90f1308479eb908e439e74d225cacb1225cd
mhjoy99 <75517252+MHJoy99@users.noreply.github.com>
2022-09-01 12:17:51 +0600

    Update and rename sample_info.py to info.py

---
+++ info.py
@@ -0,0 +1,30 @@
+# Bot information
+SESSION = 'LuciferMoringstar_Robot'
+USER_SESSION = 'User_Bot'
+API_ID = 7644705
+API_HASH = 'c134230fcb7004f5a9f31c772af5f29f'
+BOT_TOKEN = '5411942165:AAFvvhf-PkbG2o9BAPF2ORUaqFxMtq2a7B8'
+USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = True
+
+# Admins, Channels & Users
+ADMINS = [12345789, 987654321, 1006098851, 1282644258, 1940428147, 1101892352]
+CHANNELS = [-1001377437056, -1001674775818]
+AUTH_USERS = [1006098851, 1282644258]
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://mhjoy99:Mokaromjoy1234*@cluster0.shbxu.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'LuciferMoringstar_Robot'
+COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
+
+# Messages
+START_MSG = """
+**Hi, I'm MOTION PICTURE FILES BOT With Greater Abilities**
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @MotionPicturerequest2 to use this bot'
